788cf377dba555bd37413040e36c822a84010bf9
Filip Malejki <Filip.Malejki@ibm.com>
2026-03-05 16:24:12 +0100

    lab1: Java -> Python UDP communication

---
+++ lab1/lab_gniazda/java_python_communication/JavaUdpServer.java
@@ -0,0 +1,40 @@
+package java_python_communication;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.Arrays;
+
+public class JavaUdpServer {
+
+    public static void main(String args[])
+    {
+        System.out.println("JAVA UDP SERVER");
+        DatagramSocket socket = null;
+        int portNumber = 9008;
+
+        try{
+            socket = new DatagramSocket(portNumber);
+            byte[] receiveBuffer = new byte[1024];
+
+            while(true) {
+                Arrays.fill(receiveBuffer, (byte)0);
+                DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+                socket.receive(receivePacket);
+                String msg = new String(
+                    receivePacket.getData(), 
+                    0, 
+                    receivePacket.getLength(), 
+                    java.nio.charset.StandardCharsets.UTF_8
+                );
+                System.out.println("received msg: " + msg);
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            if (socket != null) {
+                socket.close();
+            }
+        }
+    }
+}
